5c02b8ed1613d345ef4d028be11d0ca8e5bddf80
MorozovIvan <condenser0071@yandex.ua>
2015-06-20 01:43:32 +0300

    First test 2

---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://ascari/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://ascari/');
+
+// DIR
+define('DIR_APPLICATION', 'C:\OpenServer\domains\ascari/catalog/');
+define('DIR_SYSTEM', 'C:\OpenServer\domains\ascari/system/');
+define('DIR_DATABASE', 'C:\OpenServer\domains\ascari/system/database/');
+define('DIR_LANGUAGE', 'C:\OpenServer\domains\ascari/catalog/language/');
+define('DIR_TEMPLATE', 'C:\OpenServer\domains\ascari/catalog/view/theme/');
+define('DIR_CONFIG', 'C:\OpenServer\domains\ascari/system/config/');
+define('DIR_IMAGE', 'C:\OpenServer\domains\ascari/image/');
+define('DIR_CACHE', 'C:\OpenServer\domains\ascari/system/cache/');
+define('DIR_DOWNLOAD', 'C:\OpenServer\domains\ascari/download/');
+define('DIR_LOGS', 'C:\OpenServer\domains\ascari/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'ascari_db');
+define('DB_PREFIX', 'oc_');
+?>
